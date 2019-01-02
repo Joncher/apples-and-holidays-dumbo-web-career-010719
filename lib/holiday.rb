abd60b18f70ceb3c81e94cd4f5a4e47b807f265f
John Crespi Oncher <johnoncher@gmail.com>
2019-01-02 19:09:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.map do |season, holiday|
-    puts season.to_s
+    season.to_s
+    puts season.capitalize
   end
 end
 
