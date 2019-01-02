dd4bba35a7c2d70f8ecb1831cf11478d6a660baa
John Crespi Oncher <johnoncher@gmail.com>
2019-01-02 19:01:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,13 +35,13 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
-  binding.pry
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  counter = 0
+  while 
 end
 
 def all_supplies_in_holidays(holiday_hash)
